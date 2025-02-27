7ce16a7bc633e93d36d4f2fbb2a6e2c40d3e93d2
Thisura2001 <141469673+Thisura2001@users.noreply.github.com>
2025-02-27 11:12:28 +0530

    modify origin url

--- Server.ts
+++ Server.ts
@@ -6,7 +6,7 @@ import authRoutes, {authenticateToken} from "./Router/auth-routes";
 const app = express();
 app.use(express.json())
 app.use(cors({
-    origin: 'http://localhost:3000',
+    origin: 'http://localhost:8081',
     methods: ['GET', 'POST'],
     credentials: true,
 }));
